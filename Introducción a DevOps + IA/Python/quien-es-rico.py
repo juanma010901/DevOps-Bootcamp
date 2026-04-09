ae8ce9aaab20553a90a9826cf8ff4958c03708b2
Juan Hincapie <juanmahc0109@gmail.com>
2026-04-09 18:14:26 -0500

    Python work

---
+++ Introducción a DevOps + IA/Python/quien-es-rico.py
@@ -0,0 +1,6 @@
+name = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+
+print("--------------------------------------------------------------")
+print(f"Hola {name}, tu tienes {edad} años y eres rico.")
+print("--------------------------------------------------------------")
